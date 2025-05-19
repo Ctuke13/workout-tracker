78be5b7592834ee0d8be0047b2505aaa3bbba3ea
Chidi <chidituke@gmail.com>
2025-05-19 18:47:24 -0400

    add JWT

---
+++ src/main/java/com/chidituke/workout_tracker/dto/RegisterRequest.java
@@ -0,0 +1,21 @@
+package com.chidituke.workout_tracker.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    @NotBlank(message = "Username is required")
+    @Size(min = 5, max = 20, message = "Username must be between 5 and 20 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 40, message = "Password must be between 6 and 40 characters")
+    private String password;
+}

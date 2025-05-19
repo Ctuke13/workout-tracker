ee30ffe8ffcc80b61a0119c4253f0259d8adfdbb
Chidi <chidituke@gmail.com>
2025-05-19 19:39:03 -0400

    add UserPrincipal

---
+++ src/main/java/com/chidituke/workout_tracker/security/UserPrincipal.java
@@ -0,0 +1,59 @@
+package com.chidituke.workout_tracker.security;
+
+import com.chidituke.workout_tracker.model.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Collections;
+
+public class UserPrincipal implements UserDetails {
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    @Getter
+    public UserPrincipal(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public static UserPrincipal create(User user) {
+        return new UserPrincipal(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword()
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
